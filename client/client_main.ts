5e91d4df4f15e67ad44f4770ecb02e253afc96d6
Abhav Kedia <abhavkedia@Abhavs-MacBook-Pro.local>
2022-08-09 05:17:32 +0530

    added files

---
+++ client/client_main.ts
@@ -0,0 +1,32 @@
+/*
+This file demonstrates uploading files to an Arweave testnet. 
+*/
+
+// This testnet is API compatible with bundlr-network's javascript-client
+import Bundlr from '@bundlr-network/client';
+const fs = require('fs');
+
+// User wallet path specified in the second argument
+const user_wallet_path = process.argv[2];
+var privateKey = JSON.parse(fs.readFileSync(user_wallet_path))
+
+// Create a bundler client with the m-testnet.arweave.net server
+const bundler = new Bundlr("http://23.22.171.112:3000", "arweave", privateKey);
+
+async function main(count) {
+
+	// Upload the file "count" times (default 1)
+	for (let i=0; i<count;i++) {
+		await uploadToBundler(process.argv[3]);
+	}
+}
+
+// Upload files to the network and fetch the DataItem ID in response
+async function uploadToBundler(path) {
+  console.log("Uploading file =", path);
+  const response = await bundler.uploadFile(path);
+  console.log("DataItem id = ", response.data.id);
+}
+
+// Start the main process
+main(process.argv[4] ? Number(process.argv[4]) : 1);
